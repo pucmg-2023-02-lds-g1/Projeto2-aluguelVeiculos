2aea508b262bda67dff45b8c0f5bf7b8ea857406
Drumond <drumond.work@gmail.com>
2023-09-14 09:48:40 -0300

    inicial config

---
+++ implementacao/src/main/java/business/implementacao/ImplementacaoApplication.java
@@ -0,0 +1,13 @@
+package business.implementacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementacaoApplication.class, args);
+	}
+
+}
