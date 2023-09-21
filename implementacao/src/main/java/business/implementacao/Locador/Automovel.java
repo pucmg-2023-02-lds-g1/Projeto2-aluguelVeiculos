d281ae2f63b3d44f616484dc684dbaa11cb03310
GiovanniBSDuarte <giovannibsduarte@gmail.com>
2023-09-21 16:57:33 -0300

    criação da pasta e arquivos locador, pequena adiçao ao cliente

---
+++ implementacao/src/main/java/business/implementacao/Locador/Automovel.java
@@ -0,0 +1,10 @@
+package business.implementacao.Locador;
+
+public class Automovel {
+    private String matricula;
+    private int ano;
+    private String marca;
+    private String modelo;
+    private String placa;
+    //private IPropietario propietario;
+}
