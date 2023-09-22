e20eb1d1db66581289bc785ca19fe8596b4391cb
PedroRamos111 <104322803+PedroRamos111@users.noreply.github.com>
2023-09-22 19:05:03 -0300

    correção crud automovel

--- implementacao/src/main/java/business/implementacao/Controllers/AutomovelController.java
+++ implementacao/src/main/java/business/implementacao/Controllers/AutomovelController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("/Automovels")
+@RequestMapping("/automoveis")
 public class AutomovelController {
 
     @Autowired
@@ -20,33 +20,33 @@ public class AutomovelController {
 
     @GetMapping
     public String listarAutomovels(Model model) {
-        List<Automovel> Automovels = AutomovelRepository.findAll();
-        model.addAttribute("Automovels", Automovels);
-        return "lista-Automovels";
+        List<Automovel> automoveis = AutomovelRepository.findAll();
+        model.addAttribute("automoveis", automoveis);
+        return "lista-automoveis";
     }
 
     @GetMapping("/novo")
     public String mostrarFormularioNovoAutomovel(Model model) {
-        model.addAttribute("Automovel", new Automovel());
-        return "formulario-Automovel";
+        model.addAttribute("automovel", new Automovel());
+        return "formulario-automovel";
     }
 
     @PostMapping("/salvar")
-    public String salvarAutomovel(@ModelAttribute("Automovel") Automovel Automovel) {
+    public String salvarAutomovel(@ModelAttribute("automovel") Automovel Automovel) {
         AutomovelRepository.save(Automovel);
-        return "redirect:/Automovels";
+        return "redirect:/automoveis";
     }
 
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEditarAutomovel(@PathVariable Long id, Model model) {
-        Automovel Automovel = AutomovelRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("ID de Automovel inválido"));
-        model.addAttribute("Automovel", Automovel);
+        Automovel automovel = AutomovelRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("ID de Automovel inválido"));
+        model.addAttribute("automovel", automovel);
         return "formulario-Automovel";
     }
 
     @GetMapping("/excluir/{id}")
     public String excluirAutomovel(@PathVariable Long id) {
         AutomovelRepository.deleteById(id);
-        return "redirect:/Automovels";
+        return "redirect:/automovels";
     }
 }
